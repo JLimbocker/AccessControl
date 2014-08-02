3c6902b83960d5855e31546ca9fbda8dbe391fef
Eric Johnson <ericmjohnson1991@msn.com>
2014-08-02 14:47:30 -0500

    Database Connector class
    **Untested**
    Connect to DB and check for gym access and access to particular tool.

---
+++ dbAccess.py
@@ -0,0 +1,62 @@
+import datetime
+import mysql.connector
+
+class dbController:
+
+	dbConfig = {
+		'user' :'labtdba',
+		'password' : 'LabT6546',
+		'host': 'engrwww1.seas.smu.edu',
+		'database' : 'labtrackdb',
+	}
+	def __init__(self):
+		
+		self.connected = False
+
+	def connect:
+		try:
+			cnx = mysql.connector.connect(**dbConfig)
+
+		except mysql.connector.Error as err:
+			if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+				print("Something is wrong with your user name or password")
+			elif err.errno == errorcode.ER_BAD_DB_ERROR:
+				print("Database does not exists")
+			else:
+				print(err)
+
+		else:
+			self.connected = True
+
+	def isConnected:
+		return self.connected
+
+	def isAllowed(student_id, f_name, l_name, tool_id):
+		cursor = cnx.cursor()
+		 
+
+		query = ("SELECT fname, lname FROM users WHERE id_number = %d AND verified = 1")
+		cursor.execute(query, student_id)
+		if cursor.rowCount == 0:
+			return False
+		elif cursor.rowCount > 1:
+			return False
+		elif cursor.rowCount == 1 and hasTraining(student_id, tool_id):
+			return True
+		else:
+			return False
+
+	def hasTraining(student_id, tool_id):
+		cursor = cnx.cursor()
+
+		query = ("SELECT fname, lname FROM users JOIN allowed_tools WHERE tool_id = %d AND id_number = %d")
+		cursor.execute(query, (tool_id, student_id))
+		if cursor.rowCount == 0:
+			return False
+		elif cursor.rowCount > 1:
+			return False
+		else:
+			return True
+
+
+
