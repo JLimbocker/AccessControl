298146db328926b2ce224a67763ba5d837758d64
Jorge Valdez <valdez.a.jorge@gmail.com>
2015-02-06 20:09:31 -0600

    Fixing errors and cleaning.

--- src/3dprint.py
+++ src/3dprint.py
@@ -3,58 +3,65 @@ import getpass
 import sys
 from collections import namedtuple
 import datetime
-#import dbAccess
+import LabTrakCardReader
+# import dbAccess
 
-#LabTrak tool ID for the 3D Printer
-printerToolID =
+# LabTrak tool ID for the 3D Printer
+printerToolID
+
 
 def main():
-	while(True):
-		user = readCard()
+    while(True):
+        user = readCard()
 
-		#Execute printer software if user exists.
-		#readCard() would return an exception anyways.
-		if user is not "":
-			print("Hello " + user.name + " " + user.surname + "\n")
-			print("ID Number: " + user.id)
+        # Execute printer software if user exists.
+        # readCard() would return an exception anyways.
+        if user is not "":
+            print("Hello " + user.name + " " + user.surname + "\n")
+            print("ID Number: " + user.id)
 
-			#Check if user is authorized to use the printer
-			#If not, then error message and send to sparkcore cloud
-			if
-			logUser(user)
-			slicer = subprocess.Popen(["C:\Program Files\PolyPrinter\KISSlicer\KISSlicer64.exe"])
-			printer = subprocess.Popen(["python", "C:\Users\install\Documents\GitHub\Printrun\pronterface.py"], shell = False, stdin = None, stdout = subprocess.PIPE, stderr = None)
+            # Check if user is authorized to use the printer
+            # If not, then error message and send to sparkcore cloud
+            if
+            logUser(user)
+            slicer = subprocess.Popen(
+                ["C:\Program Files\PolyPrinter\KISSlicer\KISSlicer64.exe"])
+            printer = subprocess.Popen(["python",
+                                        "C:\Users\install\Documents\GitHub\Printrun\pronterface.py"],
+                                       shell=False, stdin=None, stdout=subprocess.PIPE, stderr=None)
 
-			print "still going"
+            print "still going"
 
+            # Debug loop becasue the for loop below wasn't working and
+            # hates us
+            while printer.poll() is None:
+                print "in loop"
+                printer.stdout.flush()
+                line = printer.stdout.readline()
+                print "read stuff"
+                if not line:
+                    print "jk read nothing"
+                    break
+                print ">>> " + line.rstrip()
 
-			#Debug loop becasue the for loop below wasn't working and hates us
-			while printer.poll() is None:
-				print "in loop"
-				printer.stdout.flush()
-				line = printer.stdout.readline()
-				print "read stuff"
-				if not line:
-					print "jk read nothing"
-					break
-				print ">>> " + line.rstrip()
+            # Should work but doesn't print realtime
+                # for line in iter(printer.stdout.readline, b""):
+                # if "duration" in output.readline():
+                # printer.kill()
+                # print "Closed"
 
-			#Should work but doesn't print realtime
-#			for line in iter(printer.stdout.readline, b""):
-#				if "duration" in output.readline():
-#					printer.kill()
-#					print "Closed"
+                # def openDB():
+                # dbCon = dbAccess()
+                # dbCon.connect()
 
-#def openDB():
-#	dbCon = dbAccess()
-#	dbCon.connect()
 
 def logUser(user):
-	logfile = open("C:\Program Files\PolyPrinter\Logs\logs.txt", 'a')
-	time = datetime.datetime.now()
-	timestamp = time.strftime("%m/%d/%Y %H:%M:%S")
-	logfile.write("\n" + timestamp + " " + user.id + " " + user.surname + " " + user.name + " ")
-	logfile.close()
-	#This function would write the user to a text file
+    logfile = open("C:\Program Files\PolyPrinter\Logs\logs.txt", 'a')
+    time = datetime.datetime.now()
+    timestamp = time.strftime("%m/%d/%Y %H:%M:%S")
+    logfile.write(
+        "\n" + timestamp + " " + user.id + " " + user.surname + " " + user.name + " ")
+    logfile.close()
+    # This function would write the user to a text file
 
 main()
--- src/LabTrakCardReader.py
+++ src/LabTrakCardReader.py
@@ -3,120 +3,127 @@ import datetime
 import getpass
 from collections import namedtuple
 
+
 class LabTrakCardReader:
     db = LabTrak()
     db.connect()
 
-    #Reads the card swipe and returns a tuple containing each track.
-    #Raises an error if there is any misread track part.
+    # Reads the card swipe and returns a tuple containing each track.
+    # Raises an error if there is any misread track part.
     def readCard():
-    	swipe = getpass.getpass("Please swipe your ID.")
+        swipe = getpass.getpass("Please swipe your ID.")
 
-    	track1begin = 0
-    	track1end   = 0
-    	track2begin = 0
-    	track2end   = 0
-    	track3begin = 0
-    	track3end   = 0
+        track1begin = 0
+        track1end = 0
+        track2begin = 0
+        track2end = 0
+        track3begin = 0
+        track3end = 0
 
-    	for ch in range(len(swipe)):
-    		if swipe[ch] == '%':
-    			track1begin = ch + 1
-    		elif swipe[ch] == ';':
-    			track2begin = ch + 1
-    		elif swipe[ch] == '+':
-    			track3begin = ch + 1
-    		if swipe[ch] == '?':
-    			if track1end == 0 and track1begin != 0:
-    				track1end = ch
-    			elif track2end == 0 and track2begin != 0:
-    				track2end = ch
-    			elif track3end == 0 and track3begin != 0:
-    				track3end = ch
+        for ch in range(len(swipe)):
+            if swipe[ch] == '%':
+                track1begin = ch + 1
+            elif swipe[ch] == ';':
+                track2begin = ch + 1
+            elif swipe[ch] == '+':
+                track3begin = ch + 1
+            if swipe[ch] == '?':
+                if track1end == 0 and track1begin != 0:
+                    track1end = ch
+                elif track2end == 0 and track2begin != 0:
+                    track2end = ch
+                elif track3end == 0 and track3begin != 0:
+                    track3end = ch
 
-    	track1 = swipe[track1begin:track1end]
-    	track2 = swipe[track2begin:track2end]
-    	track3 = swipe[track3begin:track3end]
+        track1 = swipe[track1begin:track1end]
+        track2 = swipe[track2begin:track2end]
+        track3 = swipe[track3begin:track3end]
 
-    	if not (track1 or track2 or track3):
-    		raise ReadError()
+        if not (track1 or track2 or track3):
+            raise ReadError()
 
         return (track1, track2, track3)
 
-    #Accepts three tracks (from readCard) and generates a named tuple with
-    #User information for querying.
-    #The debug flag prints out what we pulled from the track for testing
-    #purposes.
+    # Accepts three tracks (from readCard) and generates a named tuple with
+    # User information for querying.
+    # The debug flag prints out what we pulled from the track for testing
+    # purposes.
     def generateUser(track1, track2, track3, debug=False):
         _userid = 0
-    	_name = ""
-    	_surname = ""
+        _name = ""
+        _surname = ""
 
-    	#Check card edition
-    	if not track3:
-    		_userid = track2[len(track2)-8:len(track2)+2]
+        # Check card edition
+        if not track3:
+            _userid = track2[len(track2) - 8:len(track2) + 2]
 
-    	else:
-    		list = track3.split("=")
-    		_userid = list[0]
-    		_surname = list[1]
-    		_name = list[2]
-    		check = track2[len(track2)-8:len(track2)+2]
-    		#double check id number
-    		if _userid != check:
-    			raise VerificationError(_userid, check)
+        else:
+            list = track3.split("=")
+            _userid = list[0]
+            _surname = list[1]
+            _name = list[2]
+            check = track2[len(track2) - 8:len(track2) + 2]
+            # double check id number
+            if _userid != check:
+                raise VerificationError(_userid, check)
 
-    	if debug:
-        	print("Track 1: " + track1 + "\n")
-        	print("Track 2: " + track2 + "\n")
-        	print("Track 3: " + track3 + "\n")
-        	print("Name   : " + _name + " " + _surname + "\n")
-        	print("UserID : " + _userid)
+        if debug:
+            print("Track 1: " + track1 + "\n")
+            print("Track 2: " + track2 + "\n")
+            print("Track 3: " + track3 + "\n")
+            print("Name   : " + _name + " " + _surname + "\n")
+            print("UserID : " + _userid)
 
-    	User = namedtuple('User', ['name', 'surname', 'id'], verbose=False)
-    	User.name = _name
-    	User.surname = _surname
-    	User.id = _userid
-    	return User
+        User = namedtuple('User', ['name', 'surname', 'id'], verbose=False)
+        User.name = _name
+        User.surname = _surname
+        User.id = _userid
+        return User
 
-    #Takes a User namedtuple and the id of the tool the user is attempting to
-    #use and returns true if the user is allowed to use the tool and false if
-    #the user is not allowed.
+    # Takes a User namedtuple and the id of the tool the user is attempting to
+    # use and returns true if the user is allowed to use the tool and false if
+    # the user is not allowed.
     def allowed(user, tool_id):
-        return (not (db.connected())) ? False : db.allowed(user.id, user.name, user.surname, tool_id)
+        return db.allowed(user.id, user.name, user.surname, tool_id) if db.connected() else False
 
-    #Takes a User namedtuple and the id of the tool the user is attempting to
-    #use and returns true if the user has the proper training for the tool
-    #and false if the user does not.
+    # Takes a User namedtuple and the id of the tool the user is attempting to
+    # use and returns true if the user has the proper training for the tool
+    # and false if the user does not.
     def hasToolTraining(user, tool_id):
-        return (not (db.connected())) ? False : db.hasToolTraining(user.id, tool_id)
+        return db.hasToolTraining(user.id, tool_id) if db.connected() else False
 
-    #Takes a User namedtuple and returns information about the user from the
-    #database.
+    # Takes a User namedtuple and returns information about the user from the
+    # database.
     def getUserInfo(user):
-        return (not (db.connected())) ? False : db.getUserInfo(user.id)
+        return db.getUserInfo(user.id) if db.connected() else False
 
-#Exception Classes
+# Exception Classes
 
-#Base class: CardError
+# Base class: CardError
+
+
 class CardError(Exception):
     pass
 
-#Child classes
+# Child classes
 
-#ReadError: Raised when the card information is incomplete.
+# ReadError: Raised when the card information is incomplete.
+
+
 class ReadError(CardError):
+
     def __str__(self):
         return "Read Error: Could not read card, please try again."
 
-#VerificationError: Raised when the card information is duplicated or does not
-#match the given check string.
+# VerificationError: Raised when the card information is duplicated or does not
+# match the given check string.
+
+
 class VerificationError(CardError):
+
     def __init__(self, userID, check):
         self.userID = userID
         self.check = check
 
     def __str__(self):
-        return "Verification Error: Card information did not match\n" +
-        "User ID: " + self.userID + "\n" +
-        "Check: " + self.check + "\n"
+        return "Verification Error: Card information did not match\n" + "User ID: " + self.userID + "\n" + "Check: " + self.check + "\n"
